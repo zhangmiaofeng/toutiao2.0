cb40be471b26c80e057c92cd3bb86a7890c0997f
zhangmiaofeng <843119483@qq.com>
2019-08-01 19:36:20 +0800

    使用了element-ui&&vue-router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from '@/router'
 
+// 引入element组件
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+// 控制台提示信息 开发环境来提示
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,12 @@
+// 创建路由
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  // 路由规则配置
+  routes: []
+})
+
+// 导出路由
+export default router
